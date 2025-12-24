f363cae6c6fda867edd20f4903f0d4367cbd4518
Dan McVay <danielmcvaymedia@gmail.com>
2025-12-24 23:07:43 +0000

    Imported meteor into game

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ player_rect = player_surface.get_frect(center = (window_width / 2, window_height
 star_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
 star_positions = [(randint(0,window_width),randint(0,window_height)) for i in range(20)]
 
+meteor_surface = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+meteor_rect = meteor_surface.get_frect(center = (window_width / 2, window_height / 2))
+
 
 while running:
     # Event Loop
@@ -41,8 +44,7 @@ while running:
         display_surface.blit(star_surface, position)
 
     display_surface.blit(player_surface, player_rect)
-
-
+    display_surface.blit(meteor_surface, meteor_rect)
 
     pygame.display.update()
 
