359ca8f7861102abc10d4ed6f1d290bc0bbf1bf5
Dan McVay <danielmcvaymedia@gmail.com>
2025-12-24 21:58:38 +0000

    Added a title to display window

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ windowWidth, windowHeight = 1280, 720
 display_surface = pygame.display.set_mode((windowWidth,windowHeight))
 running = True
 
+# Game Title and Icon
+
+title = "Spaceship Game"
+pygame.display.set_caption(title)
+
 while running:
     # Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_surface.fill("purple")
+    display_surface.fill("darkorchid")
+
 
     # Draw the Game
     pygame.display.update()
